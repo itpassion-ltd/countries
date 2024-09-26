32c6228244b023764f881850fc6eb11c8895fab8
PHP Guus <php.guus@gmail.com>
2024-09-26 20:51:42 +0100

    * Implementing storing of all data   - Currencies: Work In Progress   - Countries: Not Started   - Flags: Not Started   - Nationalities: Not Started   - Spatial: Not Started   - Subdivisions: Not Started   - Translations: Not Started   - World Regions: Not Started * Provide tags for use with publish commands

--- src/CountriesServiceProvider.php
+++ src/CountriesServiceProvider.php
@@ -22,11 +22,11 @@ class CountriesServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__.'/../config/itpassion-ltd-countries.php' => config_path('itpassion-ltd-countries.php'),
-        ]);
+        ], 'countries-config');
 
         $this->publishesMigrations([
             __DIR__.'/../database/create_countries_table.php.stub' => database_path('migrations/'.date('Y_m_d_His').'_create_countries_table.php'),
-        ]);
+        ], 'countries-migrations');
     }
 
     public function register(): void
