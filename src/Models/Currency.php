e7023d478f59140a37210f5ec65638eb75ced7be
PHP Guus <php.guus@gmail.com>
2024-09-27 05:46:49 +0100

    * Implementing storing of all data   - Currencies: Work In Progress     + Adding htmlEntity accessor.   - Countries: Not Started   - Flags: Not Started   - Nationalities: Not Started   - Spatial: Not Started   - Subdivisions: Not Started   - Translations: Not Started   - World Regions: Not Started

--- src/Models/Currency.php
+++ src/Models/Currency.php
@@ -2,14 +2,40 @@
 
 namespace ItpassionLtd\Countries\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Currency extends Model
 {
+    /**
+     * @inheritdoc
+     */
+    protected $appends = ['html_entity'];
+
+    /**
+     * @inheritdoc
+     */
     protected $fillable = ['alpha_3', 'minor_name', 'minor_symbol', 'minor_unit', 'major_name', 'major_symbol', 'numeric'];
 
+    /**
+     * @inheritdoc
+     */
     public function getTable()
     {
         return config('itpassion-ltd-countries.table_prefix').'currencies';
+    }
+
+    /**
+     * Get the htmlEntity for the Unicode symbol
+     * @return Attribute
+     */
+    protected function htmlEntity(): Attribute
+    {
+        return Attribute::make(
+            get: function (string $value) {
+                return Str::replace('/\\u([0-9a-fA-F]{4})/', '&#x$1;', $this->major_symbol);
+            }
+        );
     }
 }
