df5d45c40b1da03040b2c6326386383e4169f01d
PHP Guus <php.guus@gmail.com>
2024-09-27 16:50:05 +0100

    * Implementing storing of all data   - Continents:     + Added model and migration   - Countries: Not Started   - Currencies: Finished   - Flags: Not Started   - Nationalities: Not Started   - Spatial: Not Started   - Subdivisions: Not Started   - Translations: Not Started   - World Regions: Work In Progress * Publish the correct migrations by verifying the correct haystack for the right needle

--- src/CountriesServiceProvider.php
+++ src/CountriesServiceProvider.php
@@ -3,6 +3,7 @@
 namespace ItpassionLtd\Countries;
 
 use Illuminate\Foundation\Console\AboutCommand;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use ItpassionLtd\Countries\Console\Commands\Countries\Install;
@@ -21,16 +22,25 @@ class CountriesServiceProvider extends ServiceProvider
         $migrationsDirectoryName = database_path('migrations');
         $migrationsDirectory = opendir($migrationsDirectoryName);
         while ($file = readdir($migrationsDirectory)) {
+            Log::debug('Do we need to copy "'.$file.'"?');
             if ($file !== '.' || $file !== '..') {
                 if(Str::contains($file, 'continents')) {
+                    Log::debug('  <-- No');
                     unset($migrations[__DIR__.'/../database/create_continents_table.php.stub']);
                 } elseif(Str::contains($file, 'currencies')) {
+                    Log::debug('  <-- No');
                     unset($migrations[__DIR__ . '/../database/create_currencies_table.php.stub']);
                 } elseif(Str::contains($file, 'regions')) {
+                    Log::debug('  <-- No');
                     unset($migrations[__DIR__.'/../database/create_regions_table.php.stub']);
+                } else {
+                    Log::debug('  <-- Yes');
                 }
             }
         }
+        closedir($migrationsDirectory);
+
+        $this->publishesMigrations($migrations, 'countries-migrations');
     }
 
     public function boot(): void
@@ -49,12 +59,6 @@ class CountriesServiceProvider extends ServiceProvider
         ], 'countries-config');
 
         $this->advertiseUnpublishedMigrations();
-
-        $this->publishesMigrations([
-            __DIR__.'/../database/create_continents_table.php.stub' => database_path('migrations/'.date('Y_m_d_His').'_create_continents_table.php'),
-            __DIR__.'/../database/create_currencies_table.php.stub' => database_path('migrations/'.date('Y_m_d_His').'_create_currencies_table.php'),
-            __DIR__.'/../database/create_regions_table.php.stub' => database_path('migrations/'.date('Y_m_d_His').'_create_regionss_table.php'),
-        ], 'countries-migrations');
     }
 
     public function register(): void
