e32d191ca2bb16293ab28ea922016bba4b516e5f
PHP Guus <php.guus@gmail.com>
2024-09-28 04:38:57 +0100

    * Implementing storing of all data   - Continents: Work In Progress     + Implemented storing the data based on package json file   - Countries: Not Started   - Currencies: Finished   - Flags: Not Started   - Nationalities: Not Started   - Spatial: Not Started   - Subdivisions: Not Started   - Translations: Not Started   - World Regions: Work In Progress     + Implemented storing the data based on package json file

--- src/Concerns/StoreData.php
+++ src/Concerns/StoreData.php
@@ -3,11 +3,34 @@
 namespace ItpassionLtd\Countries\Concerns;
 
 use Illuminate\Support\Facades\Log;
+use ItpassionLtd\Countries\Models\Continent;
 use ItpassionLtd\Countries\Models\Currency;
 use ItpassionLtd\Countries\Models\Region;
 
 trait StoreData
 {
+    /**
+     * Store the continent data in the database.
+     *
+     * @return void
+     */
+    protected function storeContinents():void
+    {
+        $this->output->write('         Storing continents ... ');
+        $start = microtime(true);
+
+        $allContinentsStr = file_get_contents(__DIR__.'/../../data/all_continents.json');
+        $allContinentsJson = json_decode($allContinentsStr, true);
+        foreach($allContinentsJson as $continentJson) {
+            Continent::updateOrCreate([
+                'name' => $continentJson['name'],
+            ]);
+        }
+
+        $duration = microtime(true) - $start;
+        $this->output->writeLn('done ('.$duration.'s)');
+    }
+
     /**
      * Store the currency data in the database.
      *
@@ -46,79 +69,28 @@ trait StoreData
     /**
      * Store the region data in the database.
      *
-     * @param string $baseDirectory
      * @return void
      */
-    protected function storeRegions(string $baseDirectory):void
+    protected function storeRegions():void
     {
         $this->output->write('         Storing regions ... ');
         $start = microtime(true);
 
-        $worldRegion = Region::updateOrCreate([
-            'name' => 'World',
-        ], [
-            'parent_region_id' => null,
-            'un_numeric' => '001',
-        ]);
-
-        $countriesDirectoryName = $baseDirectory.'/src/data/countries/default';
-        $countriesDirectory = opendir($countriesDirectoryName);
-        while($fileName = readdir($countriesDirectory)) {
-            if($fileName !== '.' && $fileName !== '..' && $fileName !== '_all_countries.json') {
-
-                Log::debug('Opening file "' . $countriesDirectoryName . '/' . $fileName . '".');
-                $jsonString = file_get_contents($countriesDirectoryName . '/' . $fileName);
-                $countryJson = json_decode($jsonString, true);
-
-                $intermediateRegion = null;
-                $region = null;
-                $subRegion = null;
-                $theaterRegion = null;
-
-                $theaterRegionStr = $countryJson['geo']['world_region'] ?? null;
-                if($theaterRegionStr !== '' && $theaterRegionStr !== null) {
-                    $theaterRegion = Region::updateOrCreate([
-                        'name' => $theaterRegionStr,
-                        'parent_region_id' => $worldRegion->id,
-                        'un_numeric' => null,
-                    ]);
-                }
-
-                $regionStr = $countryJson['geo']['region'];
-                $regionCodeStr = $countryJson['geo']['region_code'] ?? null;
-                if($regionStr !== '' && $regionStr !== null) {
-                    $region = Region::updateOrCreate([
-                        'name' => $regionStr,
-                    ], [
-                        'parent_region_id' => $theaterRegion !== null ? $theaterRegion->id : $worldRegion->id,
-                        'un_numeric' => $regionCodeStr,
-                    ]);
-                }
-
-                $intermediateRegionStr = $countryJson['geo']['region_wb'];
-                if($intermediateRegionStr !== '' && $intermediateRegionStr !== null) {
-                    $intermediateRegion = Region::updateOrCreate([
-                        'name' => $intermediateRegionStr,
-                    ], [
-                        'parent_region_id' => $region !== null ? $region->id : ($theaterRegion !== null ? $theaterRegion->id : $worldRegion->id),
-                        'un_numeric' => null,
-                    ]);
-                }
-
-                $subRegionStr = $countryJson['geo']['subregion'];
-                $subRegionCodeStr = $countryJson['geo']['subregion_code'] ?? null;
-                if($subRegionStr !== '' && $subRegionStr !== null) {
-                    $subRegion = Region::updateOrCreate([
-                        'name' => $subRegionStr,
-                    ], [
-                        'parent_region_id' => $intermediateRegion !== null ? $intermediateRegion->id :
-                            ($region !== null ? $region->id : ($theaterRegion !== null ? $theaterRegion->id : $worldRegion->id)),
-                        'un_numeric' => $subRegionCodeStr,
-                    ]);
-                }
+        $allRegionsStr = file_get_contents(__DIR__.'/../../data/all_regions.json');
+        $allRegionsJson = json_decode($allRegionsStr, true);
+        foreach($allRegionsJson as $regionJson) {
+            $parentRegion = null;
+            $parentRegionName = $regionJson['parent_region_name'] ?? null;
+            if($parentRegionName) {
+                $parentRegion = Region::whereName($parentRegionName)->first();
             }
+            Region::updateOrCreate([
+                'name' => $regionJson['name'],
+            ], [
+                'parent_region_id' => $parentRegion->id ?? null,
+                'un_numeric' => $regionJson['un_numeric'] ?? null,
+            ]);
         }
-        closedir($countriesDirectory);
 
         $duration = microtime(true) - $start;
         $this->output->writeLn('done ('.$duration.'s)');
@@ -132,7 +104,8 @@ trait StoreData
      */
     protected function storeData(string $baseDirectory): void
     {
+        $this->storeContinents();
         $this->storeCurrencies($baseDirectory);
-        $this->storeRegions($baseDirectory);
+        $this->storeRegions();
     }
 }
