2218d436c56a3f464b24f62e3346b97652519df0
PHP Guus <php.guus@gmail.com>
2024-11-12 17:45:51 +0100

    * Remove incoming $value from the accessor.

--- src/Models/Currency.php
+++ src/Models/Currency.php
@@ -25,7 +25,7 @@ class Currency extends Model
     protected function htmlEntity(): Attribute
     {
         return Attribute::make(
-            get: function (?string $value = null) {
+            get: function () {
                 return Str::replace('/\\u([0-9a-fA-F]{4})/', '&#x$1;', $this->major_symbol);
             }
         );
